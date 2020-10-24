0cd1aea46a2a1b90622bbf92b89225d5ae1c130d
putrairawan992 <putrairawan992@yahoo.com>
2020-10-24 22:53:42 +0700

    Updates

---
+++ precache-manifest.154b7c2699b65a6fbeedd99ddf81dd64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f53dca6b25ae71cd4e785f93941f4e",
+    "url": "/pos-front-end/index.html"
+  },
+  {
+    "revision": "287a59db15001a29a44d",
+    "url": "/pos-front-end/static/css/main.8e11d4c9.chunk.css"
+  },
+  {
+    "revision": "ec4445b4d312379ac577",
+    "url": "/pos-front-end/static/js/2.c6fc5ff3.chunk.js"
+  },
+  {
+    "revision": "b425ae70647b4fb7e3b524da526f0821",
+    "url": "/pos-front-end/static/js/2.c6fc5ff3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "287a59db15001a29a44d",
+    "url": "/pos-front-end/static/js/main.0b675125.chunk.js"
+  },
+  {
+    "revision": "1730408df9bef5c59b14",
+    "url": "/pos-front-end/static/js/runtime-main.2b81392c.js"
+  }
+]);
